a027e7eb8b1b464cd8edeffc745d4048314e82cd
Gogs <gogs@fake.local>
2021-05-27 15:22:18 +0800

    other

--- web/src/main/java/mofa/wangzhe/web/ReactiveApplication.java
+++ web/src/main/java/mofa/wangzhe/web/ReactiveApplication.java
@@ -2,7 +2,6 @@ package mofa.wangzhe.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScans;
 
 /**
  * springboot guanfang
